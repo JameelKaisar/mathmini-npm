787a6238090912ed56701a497d3879dea469c0b5
Jameel Kaisar <65322473+JameelKaisar@users.noreply.github.com>
2023-09-22 22:08:52 +0000

    Add mathmini package

---
+++ tests/tests.js
@@ -0,0 +1,20 @@
+const { expect } = require('chai');
+const mathmini = require("..");
+
+describe('Testing add function', () => {
+	it('add(10, 5) should return 15', () => {
+         	expect(mathmini.add(10, 5)).to.equal(15);
+	});
+    it('add(10, 0) should return 10', () => {
+        expect(mathmini.add(10, 0)).to.equal(10);
+    });
+});
+
+describe('Testing sub function', () => {
+	it('sub(10, 5) should return 5', () => {
+         	expect(mathmini.sub(10, 5)).to.equal(5);
+	});
+    it('sub(10, 0) should return 10', () => {
+        expect(mathmini.sub(10, 0)).to.equal(10);
+    });
+});
